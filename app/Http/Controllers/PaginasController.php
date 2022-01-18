c2f3c1880063d3281ed443bf7adf88a68e99be54
MSarpi <miguel.sarpi@gmail.com>
2022-01-18 00:30:12 -0300

    prueba

---
+++ app/Http/Controllers/PaginasController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+class PaginasController extends Controller
+{
+    public function test(){
+        return view('app');
+    }
+}
